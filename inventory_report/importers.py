176237da2afc24b33a0a9225abfb8cdd103f16db
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-24 20:13:01 -0400

    :sparkles: [feat] jsonImporter-CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,7 +1,8 @@
 from typing import Dict, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
-
+import json
+import csv
 class Importer(ABC):
     def __init__(self, path: str) -> None:
         self.path = path
@@ -9,12 +10,25 @@ class Importer(ABC):
     def import_data(self) -> list[Product]:
         pass
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path) as file:
+            data = json.load(file)
+            products = []
+            for product in data:
+                products.append(Product(**product))
+            return products
+    
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path) as file:
+            data = csv.DictReader(file)
+            products = []
+            for product in data:
+                products.append(Product(**product))
+            return products
 
 
 # Não altere a variável abaixo
@@ -23,3 +37,5 @@ IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
     "csv": CsvImporter,
 }
+
+
