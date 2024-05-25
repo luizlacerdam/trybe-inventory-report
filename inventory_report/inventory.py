cda1a762caf9ce20eaace31fee1d5a43f1f359a3
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-24 21:07:42 -0400

    :sparkles: [feat] inventory-class

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from inventory_report.product import Product
+from typing import List, Union
 class Inventory:
-    pass
+    def __init__(self, data:  Union[list[Product], None] = None) -> None:
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
+    
+    def add_data(self, data: list[Product]) -> None:
+        self._data.extend(data)
